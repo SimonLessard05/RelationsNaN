d767a826b9bd5e30609dcf8ccb80b4f908b16119
Simon <2293121@cegepmontpetit.ca>
2025-01-29 22:52:31 -0500

    Progres Cours 2.1

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,4 +1,7 @@
-﻿namespace RelationsNaN.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using RelationsNaN.Migrations;
+
+namespace RelationsNaN.Models
 {
     public class Game
     {
@@ -6,5 +9,12 @@
         public string Name { get; set; }
         public string Image { get; set; }
         public int ReleaseYear { get; set; }
+
+        public int? GenreId { get; set; }
+        [ValidateNever]
+        public Genre? Genre { get; set; }
+
+        [ValidateNever]
+        public List<Platform> Platforms { get; set; }
     }
 }
